c7863e48c90ff4dd1bd0e5f668058e08986607ab
Patricio Isama <patricio_91@live.se>
2021-12-24 13:30:59 +0100

    profile stuff PRT dependen

--- src/main/java/com/salsamasterapi/salsamasterapi/Prueba.java
+++ src/main/java/com/salsamasterapi/salsamasterapi/Prueba.java
@@ -15,7 +15,7 @@ public class Prueba {
     {
         System.out.println("SVENSKAAAAAAAAAAAAA !!");
         List<Employee> arr= new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 5; i++) {
             Employee o= new Employee(i,"name "+i,"","");
             arr.add(o);
         }
